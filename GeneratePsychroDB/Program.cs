8996530ea8e0816693981881227f05dcf447853f
Vincent Cadoret <vinnividivicci@users.noreply.github.com>
2022-01-25 19:43:20 -0500

    Update Program.cs

--- GeneratePsychroDB/Program.cs
+++ GeneratePsychroDB/Program.cs
@@ -23,7 +23,7 @@ var p1 = new PsychrometricPointIP();
 //}
 
 // From 1% RH to 100% RH
-decimal j = (decimal)0.01;
+decimal j = (decimal)0.1;
 while (j <= (decimal)1.0)
 {
     // From 20 F to 120F
@@ -35,10 +35,10 @@ while (j <= (decimal)1.0)
         var newLine = $"line_rh_{j}, {p1.DryBulbTemperatureInDegF}, {p1.RelativeHumidityDecimal}, {p1.WetBulbTemperatureInDegF}, {p1.HumidityRatioGrainsPerPound}";
         csv.AppendLine(newLine);
     }
-    j += (decimal)0.01;
+    j += (decimal)0.1;
 }
 
-var filePath = "C:\\Users\\vince\\Desktop\\test.csv";
+var filePath = "C:\\Users\\vince\\Desktop\\psychroDB.csv";
 File.WriteAllText(filePath, csv.ToString());
 
 
